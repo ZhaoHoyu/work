c1596b00068f161dc427fb722d21d3c2f1124851
ZhaoHoyu <99521289+ZhaoHoyu@users.noreply.github.com>
2022-12-02 13:57:21 +0800

    Add files via upload

---
+++ data/eswin.jl
@@ -0,0 +1,46 @@
+using CSV
+using DataFrames,Statistics,Dates
+# DelimitedFiles,StringEncodings
+
+# savepath = raw"C:\Users\F1_001\Desktop\ZJ\tst.xlsx"
+filepath = raw"D:\data\FPGA_DEMURA\2"
+# data = CSV.read(raw"D:\data\FPGA_DEMURA\ESWIN\#1\t\G32.csv",DataFrame,header=0)
+datalist = readdir(filepath)
+
+function datadeal(file)
+    data = CSV.read(file,DataFrame,header=0)
+    h,w = size(data)
+    for i in 1:1:85
+        for j in 1:1:w
+            # println(data[i+100,j])
+            data[i,j] = data[i+100,j]
+            data[h-i+1,j] = data[h-i-100,j]
+        end
+    end
+    CSV.write(file,data;header=false)    
+end
+
+t1 = Dates.now()
+c = 0
+for filename in datalist
+    file = joinpath(filepath,filename)
+    if occursin(".csv",filename)
+        datadeal(file)
+        # data = CSV.read(file,DataFrame,header=0)
+        # h,w = size(data)
+        # for i in 1:1:85
+        #     for j in 1:1:w
+        #         # println(data[i+100,j])
+        #         data[i,j] = data[i+100,j]
+        #         data[h-i+1,j] = data[h-i-100,j]
+        #     end
+        # end
+        # CSV.write(file,data;header=false)
+    else ispath(file)
+        subfile = readdir(file)
+        for subfilename in subfile
+            file_ = joinpath(file,subfilename)
+            datadeal(file_)
+        end
+    end
+end
