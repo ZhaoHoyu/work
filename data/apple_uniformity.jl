e648e3a8a535c440ffc080b6f6d4777c0cbc7dd8
ZhaoHoyu <99521289+ZhaoHoyu@users.noreply.github.com>
2022-12-12 14:42:45 +0800

    Update apple_uniformity.jl
    update long edge and short edge algorithm

--- data/apple_uniformity.jl
+++ data/apple_uniformity.jl
@@ -12,12 +12,12 @@ function dyduv(data)
     Lv_list = []
     uv_list = []
     for i in 1:160
-        x1 = Float64(data[i,3])
+        x1 = data[i,3]
         # println(typeof(x1))
-        y1 = Float64(data[i,4])
+        y1 = data[i,4]
         u1 = 4*x1/(-2*x1+12*y1+3)
         v1 = 9*y1/(-2*x1+12*y1+3)
-        Lv = Float64(data[i,2])
+        Lv = data[i,2]
         push!(Lv_list,Lv)
         for j in 1:160
             if j != i
@@ -29,16 +29,18 @@ function dyduv(data)
                 push!(uv_list,uv)
             end
         end
+    end
     min_Lv = findmin(Lv_list)[1]
     max_Lv = findmax(Lv_list)[1]
     dY = min_Lv[1]/max_Lv[1]
+    # println(size(Lv_list))
+    # println(min_Lv,max_Lv,dY)
     duv = findmax(uv_list)[1]
     # sheet[2+c,2] = dY
     # sheet[2+c,3] = duv[1]
     # sheet[2+c,4] = min_Lv[1]
     # sheet[2+c,5] = max_Lv[1]
     return dY,duv,min_Lv,max_Lv
-    end
 end
 
 function duvWRTCenter(data)
@@ -109,7 +111,7 @@ function duvWorstNeighbor(data)
             vy = 9*yy/(-2*xx+12*yy+3)/4
             duvx = sqrt((ux-uc)^2+(vy-vc)^2)
             if duvx >= duvWorstNeighbor
-                Float64(duvWorstNeighbor) = duvx
+                duvWorstNeighbor = duvx
                 cord = "$(corner[p])"
             end
             n += 1
@@ -117,6 +119,62 @@ function duvWorstNeighbor(data)
         end
     end
     return duvWorstNeighbor,cord
+end
+
+function shortedeg(data)
+    duv_short_edge = 0
+    shortlist = [1,16]
+    for i in shortlist
+        for j in 1:9
+            x = data[i+(j-1)*16,3]
+            y = data[i+(j-1)*16,4]
+            uc = 4*x/(-2*x+12*y+3)/4
+            vc = 9*y/(-2*x+12*y+3)/4
+            count_num =  0
+            while (10-j-count_num) > 0
+                xx = data[i+(j+count_num)*16,3]
+                yy = data[i+(j+count_num)*16,4]
+                ux = 4*xx/(-2*xx+12*yy+3)/4
+                vy = 9*yy/(-2*xx+12*yy+3)/4
+                duvx = sqrt((ux-uc)^2+(vy-vc)^2)
+                if duvx >= duv_short_edge
+                    duv_short_edge = duvx
+                    cord1 = "$(i+(j-1)*16)"
+                    cord2 = "$(i+(j+count_num)*16)"
+                end
+                count_num += 1
+            end
+        end
+    end
+    return duv_short_edge
+end
+
+function longedeg(data)
+    duv_long_edge = 0
+    longlist = [1,145]
+    for i in longlist
+        for j in 1:15
+            x = data[i+j-1,3]
+            y = data[i+j-1,4]
+            uc = 4*x/(-2*x+12*y+3)/4
+            vc = 9*y/(-2*x+12*y+3)/4
+            count_num =  0
+            while (16-j-count_num) > 0
+                xx = data[i+j+count_num,3]
+                yy = data[i+j+count_num,4]
+                ux = 4*xx/(-2*xx+12*yy+3)/4
+                vy = 9*yy/(-2*xx+12*yy+3)/4
+                duvx = sqrt((ux-uc)^2+(vy-vc)^2)
+                if duvx >= duv_long_edge
+                    duv_long_edge = duvx
+                    cord1 = "$(i+(j-1)*16)"
+                    cord2 = "$(i+(j+count_num)*16)"
+                end
+                count_num += 1
+            end
+        end
+    end
+    return duv_long_edge
 end
 
 
@@ -133,6 +191,8 @@ XLSX.openxlsx(savepath, mode="w") do xf
     sheet[1,7] = "worst neighbor duv"
     sheet[1,8] = "duvWRTCenter_coordiante"
     sheet[1,9] = "worst neighbor duv_coordiate"
+    sheet[1,10] = "shortedegduv"
+    sheet[1,11] = "longedegduv"
     c = 0
     for filename in datalist
         file = joinpath(filepath,filename)
@@ -145,6 +205,8 @@ XLSX.openxlsx(savepath, mode="w") do xf
         dY,duv,min_Lv,max_Lv = dyduv(data)
         duvWRTCenter1 = duvWRTCenter(data)
         duvWorstNeighbor1 = duvWorstNeighbor(data)
+        duv_short_edge = shortedeg(data)
+        duv_long_edge = longedeg(data)
         sheet[2+c,2] = dY
         sheet[2+c,3] = duv
         sheet[2+c,4] = min_Lv
@@ -153,6 +215,8 @@ XLSX.openxlsx(savepath, mode="w") do xf
         sheet[2+c,7] = duvWorstNeighbor1[1]
         sheet[2+c,8] = duvWRTCenter1[2]
         sheet[2+c,9] = duvWorstNeighbor1[2]
+        sheet[2+c,10] = duv_short_edge
+        sheet[2+c,11] = duv_long_edge
         c += 1
     end
     t2 = Dates.now()
