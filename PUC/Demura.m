acf21c886794acea776f1a65add72e43a28a7a2a
ZhaoHoyu <99521289+ZhaoHoyu@users.noreply.github.com>
2023-04-12 21:09:38 +0800

    Update Demura.m
    optimize algorithm，adding define matrix space

--- PUC/Demura.m
+++ PUC/Demura.m
@@ -25,7 +25,8 @@ I(:,:,3)=csvread([colorx(3),num2str(gray),'.csv']);
 Vdata_capture_R = Vdata(:,2,band);
 Vdata_capture_G = Vdata(:,3,band);
 Vdata_capture_B = Vdata(:,4,band);
-Vdata_capture = Vdata_capture_R(gray)*ones(r,c);
+Vdata_capture = NaN(r,c,3);
+Vdata_capture(:,:,1) = Vdata_capture_R(gray)*ones(r,c);
 Vdata_capture(:,:,2) = Vdata_capture_G(gray)*ones(r,c);
 Vdata_capture(:,:,3) = Vdata_capture_B(gray)*ones(r,c);
 
@@ -39,7 +40,8 @@ lum_center(3)=mean2(I(raw-m:raw+m,col-m:col+m,3));
 
 %calculate offset
 coff = (gray/255)^2.2;
-V_interpolate=interp1(gray_ideal_coefficient,Vdata(:,2,band),I(:,:,1)./lum_center(1)*coff);
+V_interpolate=NaN(r,c,3);
+V_interpolate(:,:,1)=interp1(gray_ideal_coefficient,Vdata(:,2,band),I(:,:,1)./lum_center(1)*coff);
 V_interpolate(:,:,2)=interp1(gray_ideal_coefficient,Vdata(:,3,band),I(:,:,2)./lum_center(2)*coff);
 V_interpolate(:,:,3)=interp1(gray_ideal_coefficient,Vdata(:,4,band),I(:,:,3)./lum_center(3)*coff);
 offset=V_interpolate-Vdata_capture;
@@ -47,7 +49,7 @@ save('offset.mat','offset');
 
 %% generate simulated ptn
 gray_range=0:255;
-gray_ideal_coefficient=(gray_range./255).^2.2;%ÁÁ¶È±ÈÀıÏµÊı
+gray_ideal_coefficient=(gray_range./255).^2.2;%ÃÃÂ¶ÃˆÂ±ÃˆÃ€Ã½ÃÂµÃŠÃ½
 Vdata_ini=importdata('Vh.mat');
 bandlist=1:13;
 DBV=[2175,1600,1200,650,300,200,100,90,60,30,15,10,4];
